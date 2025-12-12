bc769eb221b644ff2a10fc5472ce16847b26e593
willamil290-tech <willamil290@gmail.com>
2025-12-12 15:13:15 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
 });
